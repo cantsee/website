3bfb37bf9a7555bde84e1ab0ef8f2568863d3e72
cantsee <michaelj29@gmail.com>
2016-02-13 20:47:38 -0700

    no message

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 <html>
 <body>
 
+<style>
+body {
+    background-image: url("style/background.jpg");
+    background-repeat: repeat-y;
+}
+</style>
+
 <h1 style="color:blue;">This is a Blue Heading</h1>
 
 <img src="items/missionclock.jpg" alt="Mission Clock" style="width:304px;height:228px;">
