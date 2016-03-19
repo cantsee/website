0e621a2d8fe19023fae0771e9b62a5d9ad7fac2f
cantsee <michaelj29@gmail.com>
2016-03-18 23:17:23 -0600

    new file

---
+++ chal/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<img src="style/banner.png" alt="banner" style="width:1004px;height:228px;">
+</head>
+<body>
+
+<style>
+body {
+    background-image: url("style/background.jpg");
+}
+</style>
+
+<h1 style="color:red;">Items</h1>
+
+<p>Custom missionary count down clock, custom make to desire<p>
+
+<img src="items/missionclock.jpg" alt="Mission Clock" style="width:304px;height:228px;">
+
+<p> Price $100 as shown customization price will vary</p>
+<button>order</button>
+
+</body>
+</html>
