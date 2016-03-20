acee29ae317e996637687cd0b92fb0bb3c2086a1
cantsee <michaelj29@gmail.com>
2016-03-20 10:42:49 -0600

    no message

---
+++ chal/setstart.php
@@ -0,0 +1,7 @@
+<?php
+	$currentStateInfo['michael_jeni'] = 0;
+	$currentStateInfo['hanah_jon'] = 0;
+	$currentStateInfo['danny_lisa'] = 0;
+	$newStateString = convertToString($currentStateInfo);
+	file_put_contents("points.txt", $newStateString);
+?>
