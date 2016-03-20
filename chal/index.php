0edf171e960817d6fb0aea4132e352d78c2a0c94
cantsee <michaelj29@gmail.com>
2016-03-20 10:01:48 -0600

    no message

--- chal/index.php
+++ chal/index.php
@@ -7,7 +7,7 @@ $password = "cmip23wl";
 $conn = new mysqli($servername, $username, $password);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    echo "Connected unsuccesfull";
 } 
 echo "Connected successfully";
 ?>
